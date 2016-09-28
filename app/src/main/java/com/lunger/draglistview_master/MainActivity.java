a298930a75066042c9df0cad5ad68d53b30775b0
wangbinglun <836414757@qq.com>
2016-09-28 15:45:35 +0800

    push project

---
+++ app/src/main/java/com/lunger/draglistview_master/MainActivity.java
@@ -0,0 +1,99 @@
+package com.lunger.draglistview_master;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.lunger.draglistview.DragListAdapter;
+import com.lunger.draglistview.DragListView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Lunger on 8/17 2016 11:21
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private DragListView mLv;
+    private ArrayList<String> mDatas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findView();
+        initData();
+        initDragListView();
+
+    }
+
+    private void findView() {
+        mLv = (DragListView) findViewById(R.id.lv);
+    }
+
+    private void initData() {
+        mDatas = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            mDatas.add("Category" + i);
+        }
+    }
+    private void initDragListView() {
+        mLv.setDragListAdapter(new MyAdapter(this, mDatas));
+        //设置拖拽响应回调
+        mLv.setMyDragListener(new DragListView.MyDragListener() {
+            @Override
+            public void onDragFinish(int srcPositon, int finalPosition) {
+                Toast.makeText(MainActivity.this, "beginPosition : " + srcPositon + "...endPosition : " + finalPosition, Toast.LENGTH_LONG).show();
+            }
+        });
+        //设置点击item哪个部位可触发拖拽（可不设置，默认是item任意位置长按可拖拽）
+        mLv.setDragger(R.id.iv_move);
+        //设置item悬浮背景色
+        mLv.setItemFloatColor("#A35151");
+        //设置item悬浮透明度
+        mLv.setItemFloatAlpha(0.65f);
+    }
+
+    class MyAdapter extends DragListAdapter {
+
+        public MyAdapter(Context context, ArrayList<String> arrayTitles) {
+            super(context, arrayTitles);
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View view;
+            /***
+             * 在这里尽可能每次都进行实例化新的，这样在拖拽ListView的时候不会出现错乱.
+             * 具体原因不明，不过这样经过测试，目前没有发现错乱。虽说效率不高，但是做拖拽LisView足够了。
+             */
+            view = LayoutInflater.from(MainActivity.this).inflate(
+                    R.layout.drag_list_item, null);
+
+            TextView textView = (TextView) view
+                    .findViewById(R.id.tv_name);
+            textView.setText(mDatas.get(position));
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return mDatas.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mDatas.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+    }
+}
